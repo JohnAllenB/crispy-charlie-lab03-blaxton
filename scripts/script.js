c4491ecebde3a0026a9452d163bc1f9a5f961abd
John Allen Blaxton <ja.blaxton@gmail.com>
2024-02-06 11:13:40 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       accept: ".right",
       drop: function(event, ui) {
         
-        var combinedImageUrl = "https://media.printables.com/media/prints/70102/images/764327_cfd3fbca-0d4f-497f-b837-a571c975ddc3/thumbs/inside/1280x960/jpg/large_display_s_7498044131362_70102.webp";
+        var combinedImageUrl = "https://c.tenor.com/Ogqj5PL-m9sAAAAC/tenor.gif";
         $("body").css("background-image", "url('" + combinedImageUrl + "')");
       }
     });
